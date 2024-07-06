3faece0f09f7854982449c30c070e5bfe5e3f2fd
Yuze Fu <i@xfox.me>
2024-07-06 14:31:36 +0900

    fix: book event

--- Net.Vatprc.Uniapi.UI.Event/src/routes/events/$event_id.tsx
+++ Net.Vatprc.Uniapi.UI.Event/src/routes/events/$event_id.tsx
@@ -3,8 +3,57 @@ import { CreateAirspace } from "@/components/create-airspace";
 import { CreateSlot } from "@/components/create-slot";
 import { useUser } from "@/services/auth";
 import { Button, Card, Group, Image, Stack, Table, Text, Title } from "@mantine/core";
+import { useQueryClient } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { format } from "date-fns";
+
+const EventBookingButtons = ({
+  eventId,
+  slotId,
+  canBook,
+  disableBook,
+  canUnbook,
+}: {
+  eventId: string;
+  slotId: string;
+  canBook: boolean;
+  disableBook: boolean;
+  canUnbook: boolean;
+}) => {
+  const queryClient = useQueryClient();
+  const postMutate = () => queryClient.invalidateQueries({ queryKey: ["api", "events", eventId, "slots"] });
+  const { mutate: book } = useApiPut(
+    "/api/events/{eid}/slots/{sid}/booking",
+    {
+      path: { eid: eventId, sid: slotId },
+    },
+    postMutate,
+  );
+  const { mutate: unbook } = useApiDelete(
+    "/api/events/{eid}/slots/{sid}/booking",
+    {
+      path: { eid: eventId, sid: slotId },
+    },
+    postMutate,
+  );
+
+  return (
+    <Group>
+      {canBook && (
+        <Button variant="subtle" onClick={() => book({})} disabled={disableBook}>
+          Book
+        </Button>
+      )}
+      {canUnbook && (
+        <Button variant="subtle" onClick={() => unbook({})}>
+          Unbook
+        </Button>
+      )}
+    </Group>
+  );
+};
+
+const EVENT_BOOKING_LIMIT = 1;
 
 const EventComponent = () => {
   const { event_id } = Route.useParams();
@@ -13,47 +62,34 @@ const EventComponent = () => {
   const { data: airspaces } = useApi("/api/events/{eid}/airspaces", { path: { eid: event_id } });
   const user = useUser();
 
-  const { mutate: book } = useApiPut("/api/events/{eid}/slots/{sid}/booking", {
-    path: { eid: event_id },
-  });
-  const { mutate: unbook } = useApiDelete("/api/events/{eid}/slots/{sid}/booking", {
-    path: { eid: event_id },
-  });
-
   const rows = slots?.map((element, id) => (
     <Table.Tr key={id}>
       <Table.Td>{element.airspace.name}</Table.Td>
       <Table.Td>{format(element.enter_at, "yyyy-MM-dd HH:mm zzzz")}</Table.Td>
       <Table.Td>
-        <Group>
-          {!element.booking && (
-            <Button variant="subtle" onClick={() => book({ path: { sid: element.id } })}>
-              Book
-            </Button>
-          )}
-          {element.booking?.user_id == user?.id && (
-            <Button variant="subtle" onClick={() => unbook({ path: { sid: element.id } })}>
-              Unbook
-            </Button>
-          )}
-        </Group>
+        <EventBookingButtons
+          eventId={event_id}
+          slotId={element.id}
+          canBook={!element.booking}
+          canUnbook={element.booking?.user_id == user?.id}
+          disableBook={
+            slots.filter((slot) => slot.booking?.user_id == user?.id).length >= EVENT_BOOKING_LIMIT &&
+            element.booking?.user_id != user?.id
+          }
+        />
       </Table.Td>
     </Table.Tr>
   ));
 
   return (
     <Stack>
-      <Image
-        src="https://community.vatprc.net/uploads/default/optimized/2X/3/35599eef688f188dc6325654461f2b4353576346_2_1380x776.jpeg"
-        alt={event?.title}
-        radius="md"
-      />
+      <Image src="https://cdn.sa.net/2024/07/06/OSoUsbluV69nhCw.png" alt={event?.title} radius="md" />
       <Title order={1}>{event?.title}</Title>
       <Title order={2}>
         Slots
         <CreateSlot ml={4} eventId={event_id} />
       </Title>
-      <Table>
+      <Table highlightOnHover>
         <Table.Thead>
           <Table.Tr>
             <Table.Th>Airspace Name</Table.Th>
