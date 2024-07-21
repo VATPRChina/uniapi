c677bf0e98a6d3af70e1528ea56c6f656f7c6124
Yuze Fu <i@xfox.me>
2024-07-21 17:56:31 +0900

    feat(event): open link in new tab

--- Net.Vatprc.Uniapi.UI.Event/src/components/markdown.tsx
+++ Net.Vatprc.Uniapi.UI.Event/src/components/markdown.tsx
@@ -2,10 +2,18 @@ import { TypographyStylesProvider } from "@mantine/core";
 import dompurify from "dompurify";
 import { parse } from "marked";
 
+dompurify.addHook("afterSanitizeAttributes", (node) => {
+  // add target=_blank to a
+  if (node.nodeName.toLowerCase() === "a") {
+    node.setAttribute("target", "_blank");
+    node.setAttribute("rel", "noopener noreferrer");
+  }
+});
+
 export const Markdown = ({ children }: { children?: string }) => {
   if (!children) return null;
 
-  const html = dompurify.sanitize(parse(children, { async: false, gfm: true, breaks: true }) as string);
+  const html = dompurify.sanitize(parse(children, { async: false, gfm: true, breaks: true }) as string, {});
 
   return (
     <TypographyStylesProvider>
