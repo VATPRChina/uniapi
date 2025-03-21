a250ba3f1d7990bc8b5222c1048668c7ca609458
Yuze Fu <i@xfox.me>
2025-03-21 17:30:59 +0900

    feat: add logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { logger } from "./log";
+import { logger as honoLog } from "hono/logger";
 
 const app = new Hono();
+
+app.use(honoLog((msg, ...rest) => logger.info(msg, ...rest)));
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -13,6 +17,6 @@ serve(
     port: 3000,
   },
   (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
+    logger.info(`Server is running on http://localhost:${info.port}`);
   }
 );
