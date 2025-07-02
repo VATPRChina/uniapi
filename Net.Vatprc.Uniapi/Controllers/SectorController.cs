03728596ede3ec737188842868ccf5426500935a
Yuze Fu <i@xfox.me>
2025-07-02 20:21:02 +0900

    feat: give vatprc8 sector permission

--- Net.Vatprc.Uniapi/Controllers/SectorController.cs
+++ Net.Vatprc.Uniapi/Controllers/SectorController.cs
@@ -49,8 +49,9 @@ public class SectorController(
         var flattenRoles = FlattenRoles(roles);
         Logger.LogInformation("User {Cid} has roles {Roles}", user.Cid,
             string.Join(", ", flattenRoles.Select(r => r.Name)));
-        var hasPermission = flattenRoles.Any(r => AllowedRoles.Contains(r.Name)) ||
-            user.Cid == "1676022";
+        var hasPermission = flattenRoles.Any(r => AllowedRoles.Contains(r.Name))
+            || user.Cid == "1676022"
+            || user.Cid == "1573922";
 
         return new SectorPermissionResponse(hasPermission, flattenRoles.Any(r => r.Name == "Visiting Controller") ? "visiting_controller" : "controller");
     }
