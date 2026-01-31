dde554f3d0ee2dafb6f214030f5c75f1e45dadd7
Yuze Fu <i@xfox.me>
2026-01-31 13:35:59 +0900

    refactor: load arinc424 from s3

--- src/Adapters/Arinc424NavdataAdapter.cs
+++ src/Adapters/Arinc424NavdataAdapter.cs
@@ -1,103 +1,75 @@
+using System.Diagnostics;
+using Amazon.Runtime;
+using Amazon.S3;
 using Arinc424;
 using Arinc424.Ground;
 using Arinc424.Navigation;
 using Arinc424.Procedures;
 using Arinc424.Waypoints;
+using Microsoft.Extensions.Options;
 using Net.Vatprc.Uniapi.Services.FlightPlan;
 using Net.Vatprc.Uniapi.Services.FlightPlan.Utility;
 using Net.Vatprc.Uniapi.Utils;
 
 namespace Net.Vatprc.Uniapi.Adapters;
 
-public class Arinc424NavdataAdapter : INavdataProvider
+public class Arinc424NavdataAdapter(IOptions<Arinc424NavdataAdapter.Option> options, ActivitySource activitySource)
 {
-    protected const string Arinc424DataPath = "Data/cesfpl.pc";
-    protected Data424 Data { get; set; } = null!;
+    public Data424 Data { get; protected set; } = Data424.Create(Meta424.Create(Supplement.V18), [], out _, out _);
 
-    public bool ExistsAirwayWithFix(string ident, string fixIdent)
+    public async Task InitializeAsync(CancellationToken ct = default)
     {
-        var exists = Data.Airways
-            .Where(a => a.Identifier == ident)
-            .Any(a => a.Sequence.Any(f => f.Fix.Identifier == fixIdent));
-        return exists;
+        using var rootActivity = activitySource.StartActivity($"{nameof(Arinc424NavdataAdapter)}.{nameof(InitializeAsync)}");
+
+        string content;
+        using (var activity = activitySource.StartActivity("Load", ActivityKind.Client))
+        {
+            var option = options.Value;
+            var credentials = new BasicAWSCredentials(option.AccessKey, option.SecretKey);
+            using var s3Client = new AmazonS3Client(credentials, new AmazonS3Config
+            {
+                ServiceURL = option.ServiceUrl,
+                ForcePathStyle = true,
+            });
+            var response = await s3Client.GetObjectAsync(option.Bucket, option.ArincPath, ct);
+            using var responseStream = response.ResponseStream;
+            using var streamReader = new StreamReader(responseStream);
+            content = await streamReader.ReadToEndAsync(ct);
+        }
+
+        using (var activity = activitySource.StartActivity("Parse", ActivityKind.Internal))
+        {
+            var meta = Meta424.Create(Supplement.V18);
+            Data = Data424.Create(meta, content.Split('\n'), out var invalid, out var skipped);
+        }
     }
 
-    public Airport? FindAirport(string ident)
+    public class Option
     {
-        var airport = Data.Airports.SingleOrDefault(a => a.Identifier == ident);
-        return airport;
+        public const string LOCATION = "Navdata:Arinc424";
+
+        public required string ServiceUrl { get; set; }
+        public required string AccessKey { get; set; }
+        public required string SecretKey { get; set; }
+        public required string Bucket { get; set; }
+        public required string ArincPath { get; set; }
     }
 
-    public IEnumerable<AirwayLeg> FindAirwayLegs(string ident)
+    public static WebApplicationBuilder ConfigureOn(WebApplicationBuilder builder)
     {
-        var airways = Data.Airways.Where(a => a.Identifier == ident).ToList();
-        var legs = airways
-            .SelectMany(airway => airway.Sequence.SkipLast(1)
-                .Zip(airway.Sequence.Skip(1), (from, to) => new AirwayLeg
-                {
-                    Airway = airway,
-                    From = from,
-                    To = to,
-                    Flipped = false,
-                })
-                .ToList());
-        return legs;
+        builder.Services.Configure<Option>(builder.Configuration.GetSection(Option.LOCATION));
+        builder.Services.AddSingleton<Arinc424NavdataAdapter>();
+        builder.Services.AddHostedService<Arinc424Initializer>();
+        builder.Services.AddScoped<INavdataProvider, Arinc424NavdataProvider>();
+        return builder;
     }
 
-    public Nondirect? FindNdbNavaid(string ident, double lat, double lon)
+    public class Arinc424Initializer : IHostedService
     {
-        var ndb = Data.Nondirects
-            .Where(a => a.Identifier == ident)
-            .OrderBy(w => Geography.DistanceBetweenPoints(w.Coordinates.Latitude, w.Coordinates.Longitude, lat, lon))
-            .FirstOrDefault();
-        return ndb;
-    }
+        readonly Arinc424NavdataAdapter _adapter;
+        public Arinc424Initializer(Arinc424NavdataAdapter adapter) => _adapter = adapter;
 
-    public Departure? FindSid(string ident, string airportIdent)
-    {
-        var airport = Data.Airports.SingleOrDefault(a => a.Identifier == airportIdent);
-        if (airport == null) return null;
-
-        var sid = airport.Departures?.SingleOrDefault(p => p.Identifier == ident);
-        return sid;
-    }
-
-    public Arrival? FindStar(string ident, string airportIdent)
-    {
-        var airport = Data.Airports.SingleOrDefault(a => a.Identifier == airportIdent);
-        if (airport == null) return null;
-
-        var star = airport.Arrivals?.SingleOrDefault(p => p.Identifier == ident);
-        return star;
-    }
-
-    public Omnidirect? FindVhfNavaid(string ident, double lat, double lon)
-    {
-        var vhf = Data.Omnidirects
-            .Where(a => a.Identifier == ident)
-            .OrderBy(w => Geography.DistanceBetweenPoints(
-                w.Coordinates.Latitude, w.Coordinates.Longitude,
-                lat, lon))
-            .FirstOrDefault();
-        return vhf;
-    }
-
-    public Waypoint? FindWaypoint(string ident, double lat, double lon)
-    {
-        var waypoint = Data.EnrouteWaypoints
-            .Where(a => a.Identifier == ident)
-            .OrderBy(w => Geography.DistanceBetweenPoints(
-                w.Coordinates.Latitude, w.Coordinates.Longitude,
-                lat, lon))
-            .FirstOrDefault();
-        return waypoint;
-    }
-
-    public async Task InitializeAsync()
-    {
-        var meta = Meta424.Create(Supplement.V18);
-        var strings = await File.ReadAllLinesAsync(Arinc424DataPath);
-        var data = Data424.Create(meta, strings, out var invalid, out var skipped);
-        Data = data;
+        public Task StartAsync(CancellationToken ct) => _adapter.InitializeAsync(ct);
+        public Task StopAsync(CancellationToken ct) => Task.CompletedTask;
     }
 }
