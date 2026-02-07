afb2b11fa818675d9fb87d57754becedcb0108c0
Yuze Fu <i@xfox.me>
2026-02-08 01:36:14 +0900

    fix: migrate database

---
+++ src/Migrations/20260207163613_TrainingAddDeletedAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Net.Vatprc.Uniapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrainingAddDeletedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "deleted_at",
+                table: "training",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deleted_at",
+                table: "training");
+        }
+    }
+}
